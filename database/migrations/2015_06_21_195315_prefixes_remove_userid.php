ecdc56ffad32f7cf54ac9434b1c90525c70c821e
bossjinja <jacksonmelissar@gmail.com>
2015-06-21 15:59:28 -0400

    Prefix table changes.

--- database/migrations/2015_06_21_195315_prefixes_remove_userid.php
+++ database/migrations/2015_06_21_195315_prefixes_remove_userid.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PrefixesAddPossessives extends Migration
+class PrefixesRemoveUserid extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,7 @@ class PrefixesAddPossessives extends Migration
     public function up()
     {
         Schema::table('prefixes', function ($table) {
-            $table->enum("prefix_possessive", ["", "\'s", "\'", "s", "\'z", "z"])->after('prefix');
-            $table->enum("suffix_possessive", ["", "of", "at", "von", "vom", "de", "d\'", "no"])->after('prefix_possessive');
+            $table->dropColumn("user_id");
         });
     }
 
@@ -26,8 +25,7 @@ class PrefixesAddPossessives extends Migration
     public function down()
     {
         Schema::table('prefixes', function ($table) {
-            $table->dropColumn("prefix_possessive");
-            $table->dropColumn("suffix_possessive");
-        });
+            $table->integer('user_id')->after('id');
+        })
     }
 }
