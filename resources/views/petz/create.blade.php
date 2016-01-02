9e9f6388de4b6b31c9c980251ecb896904a0b06a
bossjinja <jacksonmelissar@gmail.com>
2016-01-02 15:18:28 -0500

    Showname computation in JS!!!

--- resources/views/petz/create.blade.php
+++ resources/views/petz/create.blade.php
@@ -11,20 +11,20 @@
     
     <div class="form-group col-md-2">
       <label for="prefix1">Prefix</label>
-      <select name="prefix1" class="form-control">
+      <select name="prefix1" id="prefix1" class="form-control sn">
         <option value="0"></option>
         @foreach ($prefixes as $prefix)
-          <option value="{{ $prefix->id }}">{{ $prefix->prefix }}</option>
+          <option value="{{ $prefix->id }}" suffix="{{ $prefix->suffix_possessive }}" prefix="{{ $prefix->prefix_possessive }}">{{ $prefix->prefix }}</option>
         @endforeach
       </select>
     </div>
       
     <div class="form-group col-md-2">
       <label for="prefix2">Prefix</label>
-      <select name="prefix2" class="form-control">
+      <select name="prefix2" id="prefix2" class="form-control sn">
         <option value="0"></option>
         @foreach ($prefixes as $prefix)
-          <option value="{{ $prefix->id }}">{{ $prefix->prefix }}</option>
+          <option value="{{ $prefix->id }}" suffix="{{ $prefix->suffix_possessive }}" prefix="{{ $prefix->prefix_possessive }}">{{ $prefix->prefix }}</option>
         @endforeach
       </select>
     </div>
@@ -32,15 +32,15 @@
     
     <div class="form-group col-md-6">
         <label for="showname">Showname</label>
-        <input type="text" name="showname" value="{{ old('showname') }}" class="form-control">
+        <input type="text" name="showname" id="showname" value="{{ old('showname') }}" class="form-control sn">
     </div>
       
     <div class="form-group col-md-2">
       <label for="suffix">Suffix</label>
-      <select name="suffix" class="form-control">
+      <select name="suffix" id="suffix" class="form-control sn">
         <option value="0"></option>
         @foreach ($prefixes as $prefix)
-          <option value="{{ $prefix->id }}">{{ $prefix->prefix }}</option>
+          <option value="{{ $prefix->id }}" suffix="{{ $prefix->suffix_possessive }}" prefix="{{ $prefix->prefix_possessive }}">{{ $prefix->prefix }}</option>
         @endforeach
       </select>
     </div>
@@ -87,5 +87,60 @@
     <div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
+    
+    <h4>Your petz showname will display as:</h4>
+    <div id="computated_showname">
+      
+    </div>
   </form>
+    
+  <script type="text/javascript">
+      $(".sn").change(function() {
+        var showname = "";
+        
+        if (($("#prefix1").val() != "0") && ($("#prefix2").val() != "0")) {
+          showname += $("#prefix1 option:selected").text()+" "+$("#prefix2 option:selected").text();
+          if ($("#prefix2 option:selected").attr('prefix')) {
+            showname += $("#prefix2 option:selected").attr('prefix')+" ";
+          }
+          else{
+            showname += " ";
+          }
+        }
+        else if ($("#prefix1").val() != "0") {
+          showname += $("#prefix1 option:selected").text();
+          
+          if ($("#prefix1 option:selected").attr('prefix')) {
+            showname += $("#prefix1 option:selected").attr('prefix')+" ";
+          }
+          else{
+            showname += " ";
+          }
+        }
+        else if ($("#prefix2").val() != "0") {
+          showname += $("#prefix2 option:selected").text();
+          
+          if ($("#prefix2 option:selected").attr('prefix')) {
+            showname += $("#prefix2 option:selected").attr('prefix')+" ";
+          }
+          else{
+            showname += " ";
+          }
+        }
+        
+        showname += $("#showname").val();
+        
+        if ($("#suffix").val()) {
+          if ($("#suffix option:selected").attr('suffix')) {
+            showname += " "+$("#suffix option:selected").attr('suffix')+" "+$("#suffix option:selected").text();
+          }
+          else{
+            showname += " "+$("#suffix option:selected").text();
+          }
+        }
+        
+        $("#computated_showname").html(showname);
+      });
+  </script>
 @endsection
+
